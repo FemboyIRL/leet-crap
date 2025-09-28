7e1d8bb7b4085f7b94294d5f2f882f0a72f4a329
FemboyIRL <gatoelluis@gmail.com>
2025-09-28 15:17:30 -0700

    Problem 14(unfinished)

---
+++ 14. Longest Common Prefix/script.js
@@ -0,0 +1,14 @@
+var longestCommonPrefix = function(strs) {
+    var longestPrefix = ""
+    let left = 0
+    let right = 0
+
+    let base = strs[1]
+
+
+
+};
+
+const strs = ["flower","flow","flight"]
+
+console.log(longestCommonPrefix(strs))
